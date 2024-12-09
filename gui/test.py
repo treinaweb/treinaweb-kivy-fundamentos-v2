7601c6caf14fd4c7df2d9dbb461f7c37f743103c
Cleyson Lima <cleysonph@gmail.com>
2024-12-09 17:10:00 -0300

    02:04 - Primeiros contatos com a Kv Language

--- gui/test.py
+++ gui/test.py
@@ -1,17 +1,11 @@
 from kivy.app import App
-from kivy.uix.button import Button
-from kivy.uix.label import Label
 from kivy.uix.boxlayout import BoxLayout
+
+
+class Primary(BoxLayout):
+    ...
 
 
 class TestApp(App):
     def build(self):
-        layout = BoxLayout(orientation="horizontal")
-
-        button = Button(text="Meu botão")
-        label = Label(text="Minha label")
-
-        layout.add_widget(label)
-        layout.add_widget(button)
-
-        return layout
+        return Primary()
