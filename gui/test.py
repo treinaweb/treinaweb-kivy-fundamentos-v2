723de5865498fc62bd3b38c7320747c35209300d
Cleyson Lima <cleysonph@gmail.com>
2024-12-09 18:00:46 -0300

    04:06 - Widgets com propriedades reativas

--- gui/test.py
+++ gui/test.py
@@ -1,10 +1,16 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
+from kivy.properties import StringProperty, NumericProperty
 
 
 class Primary(BoxLayout):
+    primary_label_text = StringProperty('Minha label')
+    primary_label_font_size = NumericProperty(20)
+
     def test(self):
         print("O botão foi clicado")
+        self.primary_label_text = 'O botão foi clicado'
+        self.primary_label_font_size += 5
 
 
 class TestApp(App):
