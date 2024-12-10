4f20fde8f6af43846796854b9379a0026cc19c03
Cleyson Lima <cleysonph@gmail.com>
2024-12-09 23:17:21 -0300

    06:13 - Inserindo clientes

--- gui/crud.py
+++ gui/crud.py
@@ -1,9 +1,21 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 
+from db import Session
+from db.models import Client
+
 
 class Screem(BoxLayout):
-    ...
+    def add_client(self):
+        name = self.ids.name.text
+        age = self.ids.age.text
+
+        with Session() as session:
+            session.add(Client(name=name, age=age))
+            session.commit()
+
+        self.ids.name.text = ''
+        self.ids.age.text = ''
 
 
 class CrudApp(App):
